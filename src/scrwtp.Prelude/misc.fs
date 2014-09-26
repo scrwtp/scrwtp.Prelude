ec7710e211b2eb6f33bd28f50f74eb27c08919ae
scrwtp <scrwtp@gmail.com>
2014-09-26 22:36:43 +0200

    Added some option utilities and misc functions from Haskell prelude.

---
+++ src/scrwtp.Prelude/misc.fs
@@ -0,0 +1,29 @@
+﻿namespace scrwtp.Prelude
+
+module Misc = 
+
+    /// Constant function. Returns the value of its first argument and drops the second.
+    let konst x _ = x
+
+    /// Returns a function f' that takes its first two arguments in the reverse order of f.
+    let flip f a b =
+        f b a
+
+    /// Increments an integer.
+    let inc = (+) 1
+    
+    /// Decrements an integer.
+    let dec = (-) 1
+
+    /// Applies f until pred holds.
+    let until pred f x =
+        let rec loop acc =
+            if pred acc 
+                then acc
+                else loop (f acc)
+        loop x
+
+    /// Calls f n times, passing in the counter value.
+    let times n f =
+        until ((=) n) (inc >> fun x -> f x; x) 0 
+        |> ignore<int>
---
+++ src/scrwtp.Prelude/option.fs
@@ -0,0 +1,35 @@
+﻿namespace scrwtp.Prelude
+
+module Option =
+
+    /// Returns the value of an option.
+    /// If the option is None, uses a provided default value.
+    let defaultTo def value = 
+        match value with
+        | Some v -> v
+        | None   -> def
+
+    /// Returns the value of an option.
+    /// If the option is None, uses the result of provided function f.
+    let defaultToFunc f value = 
+        match value with
+        | Some v -> v
+        | None   -> f ()
+
+    /// Returns the value of an option.
+    /// If the option is None, uses the value of a provided Lazy object.
+    let defaultToLazy (lz: Lazy<_>) value = 
+        match value with
+        | Some v -> v 
+        | None   -> lz.Value
+
+    /// If a is Some, returns a, else returns b.
+    /// Be careful when pipelining (order of the arguments).
+    let combine a b = 
+        match a with
+        | Some _ -> a
+        | None   -> b
+
+    /// Reduces a list of options using combine.
+    let combineAll opts =
+        List.reduce combine opts        
