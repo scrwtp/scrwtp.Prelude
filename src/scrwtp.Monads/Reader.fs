65d8484df96faad4f62e86cc3ac8bb4fbeb4cb16
scrwtp <scrwtp@gmail.com>
2014-11-16 23:40:58 +0100

    * Added scrwtp.Monads project
    * Implemented a reader monad

---
+++ src/scrwtp.Monads/Reader.fs
@@ -0,0 +1,19 @@
+﻿namespace scrwtp.Monads
+
+module ReaderMonad = 
+
+    type Reader<'a> = R of 'a    
+    
+    module Reader = 
+        let runReader (R read) env = read env
+
+        let bind f (reader: Reader<_>) = 
+            Reader<_>.R (fun env -> runReader (f (runReader reader env)) env)
+
+        let ask f = Reader<_>.R(f)
+
+    type ReaderBuilder () = 
+        member this.Bind(reader, f) = Reader.bind f reader      
+        member this.Return(v:'a) = Reader<_>.R(fun _ -> v)
+
+    let reader = ReaderBuilder()
---
+++ src/scrwtp.Monads/ReaderTest.fsx
@@ -0,0 +1,25 @@
+﻿#I @".\bin\Debug"
+#r "scrwtp.Prelude"
+
+#load "Reader.fs"
+
+open scrwtp.Prelude
+open scrwtp.Monads.ReaderMonad
+
+type TestContext = { value: int; anotherValue: string }
+
+type TestOutput = { v1: int; v2: string }
+
+module Getters =
+    let getValue : Reader<TestContext -> int> = Reader.ask (fun ctx -> ctx.value)
+    let getAnotherValue : Reader<TestContext -> string> = Reader.ask  (fun ctx -> ctx.anotherValue)
+
+let output = 
+    reader {
+        let! v1 = Getters.getValue
+        let! v2 = Getters.getAnotherValue
+
+        return { v1 = v1; v2 = v2 }
+    }
+    |> flip Reader.runReader { value = 42; anotherValue = "Doug" }
+
